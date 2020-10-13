46e91c001552669683e4100e9d3543defbaa6b1f
mcc <andi.m.mcclure@gmail.com>
2020-10-13 01:30:25 -0400

    Copyright assertion

--- src/SortedList.js
+++ src/SortedList.js
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) 2014-present, Facebook, Inc.
+ * Additions copyright (c) 2020, Andi McClure.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
